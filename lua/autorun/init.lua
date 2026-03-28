c2cdc66cc73c2a1c892ba484cfbc3dc1acd231e1
CharleneHoo <CharleneHoo@hotmail.com>
2026-03-28 10:09:34 +0800

    增加了 init

--- lua/autorun/init.lua
+++ lua/autorun/init.lua
@@ -1,8 +1,12 @@
 -- lua/autorun/init.lua
 if SERVER then
+	include("modules/LogicEntity/logic_entity.lua")
+	local LogicPlayer = include("modules/LogicEntity/logic_player.lua")
+	LogicPlayer.Init() -- register hooks
+	local LogicNPC = include("modules/LogicEntity/logic_npc.lua")
+	LogicNPC.Init() -- register hooks
 	include("modules/ProxyManager/proxy_manager.lua")
 	include("modules/ProxyManager/proxy_behavior.lua")
-	include("modules/ProxyManager/ragdoll_linker.lua")
 	include("modules/ProxyManager/proxy_monitor.lua")
 	include("modules/auto_requester.lua")
 	-- include("modules/bone_manager.lua")
